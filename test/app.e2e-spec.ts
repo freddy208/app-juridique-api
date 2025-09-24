3bd5c1cd44778d5dee0670e73e1e341900de46b0
Freddy Patient <134920187+freddy208@users.noreply.github.com>
2025-09-24 16:49:17 +0100

    Resolution 2.4 (#3)
    * Add package-lock.json for CI/CD
    
    * Test workflow
    
    * Test workflow 2
    
    * Test workflow 3
    
    * Test workflow 4
    
    * Test workflow 5
    
    * Test workflow 6
    
    * Test workflow 7
    
    * Test workflow 8
    
    * resolution 1.1
    
    * resolution 1.2
    
    * resolution 1.3
    
    * Resolution 1.4
    
    * Resolution 1.5
    
    * Resolution 1.7
    
    * Resolution 8
    
    * Resolution 1.9
    
    * resolution 2.0
    
    * Resolution 2.1
    
    * Resolution 2.2
    
    * Resolution 2.3

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import request from 'supertest'; // ✅ import par défaut
-
-import { NestExpressApplication } from '@nestjs/platform-express';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication & NestExpressApplication;
-
-import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
-
-describe('AppController (e2e)', () => {
-  let app: INestApplication & NestExpressApplication;
+  let moduleFixture: TestingModule;
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
@@ -32,30 +23,6 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', async () => {
     const response = await request(app.getHttpServer()).get('/').expect(200);
-
     expect(response.text).toBe('Hello World!');
-
-    app = moduleFixture.createNestApplication<NestExpressApplication>();
-    await app.init();
-  });
-
-  it('/ (GET)', async () => {
-    const response = await request(app.getHttpServer()).get('/').expect(200);
-    expect(response.text).toBe('Hello World!');
-  });
-
-  afterEach(async () => {
-    await app.close();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-
   });
 });
