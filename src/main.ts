8dc8d41efbe78145ed35183e23c4ac5265adb3a3
freddy208 <kfreddypatient@gmail.com>
2025-09-24 12:40:29 +0100

    Test workflow 4

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+// Gérer toutes les erreurs de démarrage proprement
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+  process.exit(1); // quitte le process si erreur
+});
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { App } from 'supertest/types';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication & NestExpressApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestExpressApplication>();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/').expect(200);
+    expect(response.text).toBe('Hello World!');
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
