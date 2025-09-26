1607d8c37acf3c2cc92491cbade394eaa4d14212
freddy208 <kfreddypatient@gmail.com>
2025-09-26 17:53:22 +0100

    Ajout de swagger v3

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,11 +6,14 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { RefreshTokenGuard } from './guards/refresh-token.guard';
 import { ForgotPasswordDto } from './dto/forgot-password.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
+import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiTags('Auth') // Regroupe tous les endpoints de ce controller dans Swagger
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Connexion utilisateur avec email et mot de passe' })
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     const user = await this.authService.validateUser(
@@ -20,42 +23,50 @@ export class AuthController {
     return this.authService.login(user);
   }
 
+  @ApiOperation({ summary: 'Déconnexion utilisateur' })
+  @ApiBearerAuth('JWT-auth')
   @UseGuards(JwtAuthGuard)
   @Post('logout')
   async logout(@Req() req: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     return this.authService.logout({ id: req.user.sub, email: req.user.email });
   }
 
-  // Endpoint pour rafraîchir le token
-  // Endpoint pour rafraîchir le token
+  @ApiOperation({ summary: 'Rafraîchir le token JWT' })
   @UseGuards(RefreshTokenGuard)
   @Post('refresh')
   refresh(@Req() req: any) {
-    // req.user est injecté automatiquement par le RefreshTokenGuard
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
     return this.authService.refreshToken(req.user);
   }
 
-  @UseGuards(JwtAuthGuard) // Seuls les connectés peuvent créer
+  @ApiOperation({ summary: 'Créer un nouvel utilisateur (admin seulement)' })
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard)
   @Post('register')
   async register(@Req() req, @Body() dto: RegisterDto) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
     return this.authService.register(req.user, dto);
   }
-  // Nouveau endpoint pour récupérer l'utilisateur connecté
+
+  @ApiOperation({ summary: 'Récupérer les informations du user connecté' })
+  @ApiBearerAuth('JWT-auth')
   @UseGuards(JwtAuthGuard)
   @Get('me')
   async me(@Req() req: any) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-    return this.authService.me(req.user.sub); // req.user.sub = id du user (payload JWT)
+    return this.authService.me(req.user.sub);
   }
-  //forgot password
+
+  @ApiOperation({
+    summary: 'Envoyer un email pour réinitialiser le mot de passe',
+  })
   @Post('forgot-password')
   async forgotPassword(@Body() body: ForgotPasswordDto) {
     return this.authService.forgotPassword(body.email);
   }
-  //reset password
+
+  @ApiOperation({ summary: 'Réinitialiser le mot de passe avec le token' })
   @Post('reset-password')
   async resetPassword(@Body() body: ResetPasswordDto) {
     return this.authService.resetPassword(body.token, body.nouveauMotDePasse);
