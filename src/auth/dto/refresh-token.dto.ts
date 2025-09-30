820e9f628f513b73f975cf01dc85e67b562fd07d
freddy208 <kfreddypatient@gmail.com>
2025-09-30 13:24:51 +0100

    Correction du endpoint refresh token v1

--- src/auth/dto/refresh-token.dto.ts
+++ src/auth/dto/refresh-token.dto.ts
@@ -3,6 +3,13 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class RefreshTokenDto {
   @ApiProperty({
+    example: 'uuid-utilisateur',
+    description: "L'identifiant de l'utilisateur",
+  })
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+  @ApiProperty({
     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
     description: 'Token de rafraîchissement JWT reçu lors de la connexion',
   })
