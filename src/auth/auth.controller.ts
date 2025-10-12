2f07c7db5ed99ae5cd1204456284f32ecccf018a
freddy208 <kfreddypatient@gmail.com>
2025-10-12 14:52:16 +0100

    Ajout simsa non

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -46,7 +46,7 @@ export class AuthController {
     res.cookie('refresh_token', refresh_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge,
     });
 
@@ -67,7 +67,7 @@ export class AuthController {
     res.clearCookie('refresh_token', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
     });
 
     return { message: 'Déconnexion réussie' };
@@ -97,7 +97,7 @@ export class AuthController {
     res.cookie('refresh_token', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
